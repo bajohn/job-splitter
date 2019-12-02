b950bc7e112b6c08090d510f9bff02acd13b5e56
Brendan John <bjohn454@gmail.com>
2019-12-01 20:50:56 -0500

    remove main.py (debug script)

--- main.py
+++
@@ -1,7 +0,0 @@
-from combinator import CombinationMapper
-
-print('init')
-mapper = CombinationMapper(3, ['a', 'b', 'c'])
-for i in range(0, 27):
-    output = mapper.getCombinationFromInt(i)
-    print(output)
