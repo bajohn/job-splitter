74cd9b6ebbb1479dbe2b456f9d63ddbeb4021b56
Brendan John <bjohn@Brendans-MBP.fios-router.home>
2019-12-17 00:12:16 -0500

    deploy script + tf file hash + tf apply works

--- script.sh
+++ script.sh
@@ -2,16 +2,18 @@
 
 # this is b/c pipenv stores the virtual env in a different
 # directory so we need to get the path to it
-SITE_PACKAGES=$(pipenv --venv)/lib/python3.6/site-packages
+SITE_PACKAGES=$(pipenv --venv)/lib/python3.7/site-packages
 echo "Library Location: $SITE_PACKAGES"
-DIR=$(pwd)
+DIR=`pwd`
 
 # Make sure pipenv is good to go
-echo "Do fresh install to make sure everything is there"
-pipenv install
+# echo "Do fresh install to make sure everything is there"
+# pipenv install
+
+zip -r9 package.zip ./lambdas
 
 cd $SITE_PACKAGES
-zip -r9 $DIR/package.zip *
+zip -rg9 $DIR/package.zip *
 
-cd $DIR
-zip -rg package.zip $DIR/lambdas/*
+
+aws s3 cp $DIR/package.zip s3://tf-changedname/package.zip
--- terraform/demo.tf
+++ terraform/demo.tf
@@ -8,6 +8,11 @@ resource "aws_s3_bucket" "tf-bucket" {
   acl    = "private"
 }
 
+data "aws_s3_bucket_object" "lambda_data" {
+  bucket = aws_s3_bucket.tf-bucket.bucket
+  key    = "package.zip"
+}
+
 # the below lambda code hasn't been deployed yet
 
 resource "aws_iam_role" "iam_for_lambda2" {
@@ -31,13 +36,15 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "../package.zip"
+  # filename      = "../package.zip"
+  s3_bucket = aws_s3_bucket.tf-bucket.bucket
+  s3_key = "package.zip"
   function_name = "created_by_terraform2"
-  role          = "${aws_iam_role.iam_for_lambda2.arn}"
-  handler       = "demo_handler.handler"
+  role          = aws_iam_role.iam_for_lambda2.arn
+  handler       = "lambdas/demo_handler.handler"
 
   runtime = "python3.7"
-
+  source_code_hash = filemd5("../package.zip")# base64encode(data.aws_s3_bucket_object.lambda_data.body)
   environment {
     variables = {
       foo = "bar"
